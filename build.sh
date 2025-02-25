c8f2a4f01b3cf6b62e7539dfb221f7604e70991f
William Bjorndahl <wmbjorndahl@gmail.com>
2025-02-25 14:18:40 -0600

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,11 @@
+"""
+git clone https://github.com/stevenlovegrove/Pangolin.git
+cd Pangolin
+mkdir build && cd build
+cmake ..
+make -j2
+sudo make install
+"""
 echo "Configuring and building Thirdparty/DBoW2 ..."
 
 cd Thirdparty/DBoW2
@@ -36,5 +44,6 @@ echo "Configuring and building ORB_SLAM3 ..."
 
 mkdir build
 cd build
-cmake .. -DCMAKE_BUILD_TYPE=Release
-make -j4
+cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_CXX_STANDARD=14 # Various issues on original repository discuss switching from C++11 to C++14.
+# For now we will specify building with C++14 at this step. Future work can do full migration across the repo.
+make -j2 # Orin Nano Super Dev Kit crashes with the original make -j4. Successfully got it working with make -j2
